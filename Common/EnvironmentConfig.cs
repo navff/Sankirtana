fec5436ac70ee1cc35951a2bc422db00f3982097
Vladimir varnavskiy <var@33kita.ru>
2022-08-19 05:23:59 +0300

    Get configs from Yandex Object Storage

--- Common/EnvironmentConfig.cs
+++ Common/EnvironmentConfig.cs
@@ -1,4 +1,7 @@
-﻿namespace Sankirtana.Web.Common;
+﻿using AspNetCore.Yandex.ObjectStorage;
+using AspNetCore.Yandex.ObjectStorage.Configuration;
+
+namespace Sankirtana.Web.Common;
 
 public class EnvironmentConfig
 {
@@ -6,10 +9,37 @@ public class EnvironmentConfig
     
     public EnvironmentConfig()
     {
-        MongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING")!;
-        if (string.IsNullOrEmpty(MongoConnectionString))
+        var yaSecretKey = Environment.GetEnvironmentVariable("YA_OBJECT_STORAGE_SECRET_KEY")!;
+        if (string.IsNullOrEmpty(yaSecretKey))
         {
-            throw new InvalidOperationException("There is no environment variable MONGO_CONNECTION_STRING");
+            throw new InvalidOperationException("There is no environment variable YA_OBJECT_STORAGE_SECRET_KEY");
         };
+        
+        var oss = new YandexStorageService(new YandexStorageOptions()
+        {
+            AccessKey = "YCAJEWT3HNKiSCnrfiXOf_Bsm",
+            SecretKey = yaSecretKey,
+            BucketName = "secrets"
+        });
+        var result = oss.ObjectService.GetAsync("sankirtana.json").Result;
+        
+        var stream = result.ReadAsStreamAsync().Result.Value;
+        var reader = new StreamReader(stream);
+        var text = reader.ReadToEnd();
+        
+        var config = Newtonsoft.Json.JsonConvert.DeserializeObject<SankirtanaConfig>(text);
+        if (config != null)
+        {
+            MongoConnectionString = config.MongoConnectionString;
+        }
+        else
+        {
+            throw new InvalidOperationException("Cannot read config file from Yandex Object Storage");
+        }
+    }
+    
+    public class SankirtanaConfig
+    {
+        public string MongoConnectionString { get; set; } = "";
     }
 }
