198ad05ac532eba89ee6b00e8149efb396d9e500
Vladimir varnavskiy <var@33kita.ru>
2022-08-20 06:15:46 +0300

    Поправил конфигурацию запуска

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using Microsoft.AspNetCore.Mvc;
 using Sankirtana.Web.Business.Book;
 using Sankirtana.Web.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT");
+port = port ?? "8080";
+builder.WebHost.UseUrls($"http://localhost:{port}");
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    // отключаем глобально Antiforgery-токен
+    options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
+
+    // Custom routes
+    // options.Conventions.AddPageRoute("/Mk/MkList", "/mk");
+});
+builder.Services.AddSingleton(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic }));
 builder.Services.AddSingleton<EnvironmentConfig>();
 builder.Services.AddSingleton<DbStore>();
 builder.Services.AddTransient<BookService>();
