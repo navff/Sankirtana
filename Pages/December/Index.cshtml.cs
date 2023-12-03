d4779d1b2f8916a515c8c42d51254dda3acaecca
navff <var@33kita.ru>
2023-12-03 07:42:33 +0300

    Добавил декабрьский график

---
+++ Pages/December/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sankirtana.Web.Business.Sales;
+
+namespace Sankirtana.Web.Pages.December;
+
+public class Index : PageModel
+{
+    private SalesService _salesService;
+    public PeriodicStatistic Statistic;
+
+
+    public Index(SalesService salesService)
+    {
+        _salesService = salesService;
+    }
+
+    public async Task OnGet()
+    {
+        var dateStart = new DateTime(2024, 12, 1);
+        var dateEnd = new DateTime(2024, 12, 31, 23, 59, 59);
+            
+        this.Statistic = await _salesService.GetSalesStat(
+            dateStart, 
+            dateEnd);         
+    }
+}
---
+++ wwwroot/js/gauge.js
@@ -0,0 +1,95 @@
+﻿function renderGauge(curentValue) {
+    var myConfig = {
+        type: "gauge",
+        globals: {
+            fontSize: 17
+        },
+        plotarea: {
+            marginTop: 80
+        },
+        plot: {
+            size: '100%',
+            valueBox: {
+                placement: 'center',
+                text: '%v', //default
+                fontSize: 35,
+                rules: [
+                    {
+                        rule: '%v >= 2154',
+                        text: '%v<br>Кришна — Бог!'
+                    },
+                    {
+                        rule: '%v < 2154 && %v > 1107',
+                        text: '%v<br>МОЛОДЦЫ!'
+                    },
+                    {
+                        rule: '%v < 1108 && %v > 1000',
+                        text: '%v<br>Ещё чуть-чуть'
+                    },
+                    {
+                        rule: '%v <  1000',
+                        text: '%v<br>Маловато'
+                    }
+                ]
+            }
+        },
+        tooltip: {
+            borderRadius: 5
+        },
+        scaleR: {
+            aperture: 300,
+            minValue: 0,
+            maxValue: 3000,
+            step: 10,
+            center: {
+                visible: false
+            },
+            tick: {
+                visible: false
+            },
+            item: {
+                visible: true,
+                offsetR: 0,
+                rules: [{
+                    rule: '%i == 9',
+                    offsetX: 15
+                }]
+            },
+            ring: {
+                size: 70,
+                rules: [
+                    {
+                        rule: '%v <= 1108',
+                        backgroundColor: '#E53935'
+                    },
+                    {
+                        rule: '%v >= 1108 && %v < 2154',
+                        backgroundColor: '#2ee7e7'
+                    },
+                    {
+                        rule: '%v >= 2154',
+                        backgroundColor: '#05b21a'
+                    }
+                ]
+            }
+        },
+        series: [{
+            values: [curentValue], // starting value
+            backgroundColor: 'black',
+            indicator: [10, 10, 10, 10, 0.75],
+            animation: {
+                effect: 2,
+                method: 1,
+                sequence: 4,
+                speed: 900
+            },
+        }]
+    };
+
+    zingchart.render({
+        id: 'myChart',
+        data: myConfig,
+        height: 500,
+        width: '100%'
+    });
+}
